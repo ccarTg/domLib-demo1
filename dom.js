8d64b62c102d4caf01dff7de3bce27b9e7b37b9b
ccarTg <68485760+ccarTg@users.noreply.github.com>
2020-11-18 10:12:15 +0000

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,171 @@
+
+window.dom = {
+    //创建节点
+    create(string) {
+        const container = document.createElement("template");
+        container.innerHTML = string.trim();
+        return container.content.firstChild;
+    },
+
+    //新增弟弟
+    after(node, node2) {
+        node.parentNode.insertBefore(node2, node.nextSibling);
+    },
+
+    //新增哥哥
+    before(node, node2) {
+        node.parentNode.insertBefore(node2, node);
+    },
+
+    //新增儿子
+    append(parent, node) {
+        parent.appendChild(node);
+    },
+
+    //新增爸爸
+    wrap(node, parent) {
+        dom.before(node, parent)
+        dom.append(parent, node)
+    },
+
+    //删节点
+    remove(node){
+        node.parentNode.removeChild(node)
+        return node
+    },
+
+    //删掉所有子节点
+    empty(node){
+        const {childNodes} = node;
+        const array = [];
+        let x = node.firstChild
+        while(x){
+            array.push(dom.remove(node.firstChild))
+            x = node.firstChild
+        }
+        return array
+    },
+
+    //读写属性
+    attr(node, name, value){//重载：参数不一样代码不一样
+        if(arguments.length === 3){
+            node.setAttribute(name, value)
+        }
+        else if(arguments.length === 2){
+            return node.getAttribute(name)
+        }
+    },
+
+    //读写文本内容
+    text(node, string){//适配
+        if(arguments.length === 2){
+            if('innerText' in node){
+                node.innerText = string
+            }
+            else{
+                node.textContent = string
+            }
+        }else if(arguments.length === 1){
+            if('innerText' in node){
+                return node.innerText
+            }
+            else{
+                return node.textContent
+            }
+        }
+    },
+
+    html(node, string){
+        if(arguments.length === 2){
+            node.innerHTML = string
+        }else if(arguments.length === 1){
+            return node.innerHTML
+        }
+    },
+
+    style(node, name, value){
+        if(arguments.length === 3){
+            node.style[name] = value
+        }else if(arguments.length === 2){
+            if(typeof name === 'string'){
+                return node.style[name]
+            }else if(name instanceof Object){
+                for(let key in name){
+                    node.style[key] = name[key]
+                }
+            }
+        }
+    },
+
+    class:{
+        add(node, className){
+            node.classList.add(className)
+        },
+        remove(node, className){
+            node.classList.remove(className)
+        },
+        has(node, className){
+            return node.classList.contains(className)
+        }
+    },
+
+    on(node, eventName, fn){
+        node.addEventListener(eventName, fn)
+    },
+
+    off(node, eventName, fn){
+        node.removeEventListener(eventName, fn)
+    },
+
+    find(selector , scope){
+        return (scope || document).querySelectorAll(selector)
+    },
+
+    parent(node){
+        return node.parentNode
+    },
+
+    children(node){
+        return node.children
+    },
+
+    siblings(node){
+        return Array.from(node.parentNode.children).filter(n=>n!==node)
+    },
+
+    next(node){
+        let x = node.nextSibling
+        while(x && x.nodeType === 3){
+            x = x.nextSibling
+        }
+        return x
+    },
+
+    previous(node){
+        let x = node.previousSibling
+        while(x && x.nodeType === 3){
+            x = x.previousSibling
+        }
+        return x
+    },
+
+    each(nodeList, fn){
+        for(let i = 0; i<nodeList.length; i++){
+            fn.call(null, nodeList[i])
+        }
+    },
+
+    index(node){
+        const list = dom.children(node.parentNode)
+        let i 
+        for(i = 0; i<list.length; i++){
+            if(list[i] === node){
+                break
+            }
+        }
+        return i
+    }
+
+}; 
+
+
